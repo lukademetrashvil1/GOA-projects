c008ab9edccd99de11578be0ee3b7a03df6e57b9
lukademetrashvil1 <demetrashvililuka9@gmail.com>
2024-05-28 21:04:19 +0400

    commit

---
+++ level 06/classwork/06.py
@@ -0,0 +1,19 @@
+nun1=input("plis enter a namber")
+nun2=input("plis enter a namber")
+
+
+print(int(nun1) + int(nun2))
+
+
+
+
+
+
+
+firstname=input("plis enter  your  name")
+lastname=input("plis enter  your  lastname")
+ege=input("plis enter  your  ege")
+gmail=input("plis enter  your  gmail")
+
+print(firstname + " " + lastname + " " + ege + " " + gmail )
+
