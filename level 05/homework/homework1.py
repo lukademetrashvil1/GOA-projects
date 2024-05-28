486bd2594b75205955a7c1536954c9ef3cb8baa7
lukademetrashvil1 <demetrashvililuka9@gmail.com>
2024-05-28 17:24:53 +0400

    commit

---
+++ level 05/homework/homework1.py
@@ -0,0 +1,9 @@
+
+name = "luka"
+surname = "demetrashvili"
+ege = 15
+qountry = "georgia"
+eddress="TBILISI"
+academy ="GOA-Oriented Academy"
+
+print(name + " " + surname + " " + str(ege) + " " + str(qountry) + " " + eddress + " " + academy)
