a1ab2fe677ade670e55126a9b75e3cd245579cfb
lukademetrashvil1 <demetrashvililuka9@gmail.com>
2024-06-11 21:35:29 +0400

    commit

---
+++ level 10/homework/hw.py
@@ -0,0 +1,20 @@
+for i in range(10):
+    print(str(i)+ "." + ">")
+for i in range(10):
+    print(str(i)+ "." "eaiof")
+for i in range(10):    
+    print(str(i)+ "." "daadsddd")
+for i in range(10):
+    print(str(i)+ "." "messi is te goat")
+for i in range(10):
+    print(str(i)+ "." "anime is on top")
+for i in range(10):
+    print(str(i)+ "." "meme's ar the beast")   
+for i in range(10):
+    print(str(i)+ "." "cANGALA gungel")
+for i in range(10):
+    print(str(i)+ "." "can i gara hoiaaaaa")
+for i in range(10):
+    print(str(i)+ "." + "why i'm stupyed")
+for i in range(10):
+    print(str(i)+ "." + "supernigaa")
